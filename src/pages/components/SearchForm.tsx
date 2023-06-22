a92c1d8a66825f9d061d7299b75569738c7fce48
Dan Nicolae Mic <micdan1994@gmail.com>
2023-06-22 14:36:46 +0300

    [MOZ-1] Implemented adition of multiple intermediate fields for cites with placeholder field component.

--- src/pages/components/SearchForm.tsx
+++ src/pages/components/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import {
   Box,
@@ -8,23 +8,28 @@ import {
   FormLabel,
   Heading,
   Input,
+  InputGroup,
+  InputRightElement,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
   NumberInputField,
   NumberInputStepper,
+  Stack,
   VStack,
 } from "@chakra-ui/react";
 import * as Yup from 'yup';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRoad, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faCity, faRoad, faSpinner, faSquareMinus } from "@fortawesome/free-solid-svg-icons";
 import { AutoComplete, AutoCompleteInput, AutoCompleteItem, AutoCompleteList } from "@choc-ui/chakra-autocomplete";
 import citiesJSON from "../../data/cities.json";
 
 interface SearchFromProps {}
 
+
 const SearchForm: React.FC<SearchFromProps> = () => {
     const isLoading: boolean = false;
+    const [ intermediateCitiesInputs, setintermediateCitiesInputs ] = useState<Map<string, Object>>(new Map());
     
     const formik = useFormik({
         initialValues: {
@@ -45,20 +50,60 @@ const SearchForm: React.FC<SearchFromProps> = () => {
           passangers: Yup.number().required('Required')
         }),
       });
-
-      useEffect(() => {
-        console.log(formik.errors);
-        
-      }, [formik.errors, formik.errors.passangers])
     
     const finalDestinationFieldProps = formik.getFieldProps('finalDestination');
     const passangersFieldProps = formik.getFieldProps('passangers');
+
+    const addIntermediateCityInput = () : void => {
+      console.log('addIntermediateCityInput is clicked');
+      let indexNumber: number = intermediateCitiesInputs.size + 1;
+      let mapKey: string = `intermediate-cityt-input-${indexNumber}`;
+      let newInput: Object = (
+        <FormControl key={mapKey} isInvalid={formik.errors.finalDestination !== undefined}>
+        <FormLabel htmlFor="email">Intermediate City</FormLabel>
+        <InputGroup>
+          <Input
+            disabled={isLoading}
+            id="finalDestination"
+            type="text"
+            placeholder="Type your destination here..."
+            {...finalDestinationFieldProps}
+          />
+          <InputRightElement
+            cursor={'pointer'}
+            onClick={() => console.log('Should delete the field')}
+            children={<FontAwesomeIcon icon={faSquareMinus} />}
+            {...{ "intermediate-city-id": `${mapKey}` }}
+          />
+        </InputGroup>
+        <FormErrorMessage>{formik.errors.finalDestination}</FormErrorMessage>
+      </FormControl>
+      );
+      setintermediateCitiesInputs(new Map(intermediateCitiesInputs.set(mapKey, newInput)));
+    }
+
+    const renderItermediateCitiesInputs = () => {
+      let inputs: Object[] = [];
+      intermediateCitiesInputs.forEach((inputComponent, key) => {
+        inputs.push(inputComponent);
+      })
+      return inputs;
+    }
+
+    useEffect(() => {
+      console.log(intermediateCitiesInputs);
+    }, [intermediateCitiesInputs, intermediateCitiesInputs.size])
       
   return (
     <VStack w="1024px" p={32} alignItems="flex-start">
     <Heading as="h1" id="contactme-section">
      <FontAwesomeIcon icon={faRoad} /> Find out how many km will take...
     </Heading>
+    <Stack direction='row' spacing={4}>
+      <Button onClick={addIntermediateCityInput} rightIcon={<FontAwesomeIcon icon={faCity} />} colorScheme='teal' variant='outline'>
+        Add an intermediate city
+      </Button>
+    </Stack>
     <Box p={6} rounded="md" w="100%">
       <form onSubmit={(e) => {
         e.preventDefault();
@@ -66,6 +111,7 @@ const SearchForm: React.FC<SearchFromProps> = () => {
       }}>
         {/* <fieldset disabled={isLoading ? "disabled" : ""} > */}
         <VStack spacing={4}>
+          {/* Start city destionation input */}
           <FormControl isInvalid={formik.errors.startCity !== undefined}>
               <FormLabel >Origin city</FormLabel>
               <AutoComplete
@@ -94,6 +140,14 @@ const SearchForm: React.FC<SearchFromProps> = () => {
                 </AutoComplete>
                 <FormErrorMessage>{formik.errors.startCity}</FormErrorMessage>
           </FormControl>
+
+          {/* Render the dynamically added inputs */}
+            <>
+              {renderItermediateCitiesInputs()}
+            </>
+          {/* End of the dynamically added inputs or in between cities  */}
+
+          {/* Final city destionation input */}
             <FormControl isInvalid={formik.errors.finalDestination !== undefined}>
               <FormLabel htmlFor="email">Final Destination</FormLabel>
               <Input
@@ -105,6 +159,8 @@ const SearchForm: React.FC<SearchFromProps> = () => {
               />
               <FormErrorMessage>{formik.errors.finalDestination}</FormErrorMessage>
             </FormControl>
+
+            {/* Passaengers number input section */}
             <FormControl isInvalid={formik.errors.passangers !== undefined}>
               <FormLabel htmlFor="passanger">Number of passangers</FormLabel>
               <NumberInput
@@ -122,7 +178,7 @@ const SearchForm: React.FC<SearchFromProps> = () => {
               </NumberInput>
               <FormErrorMessage>{formik.errors.passangers}</FormErrorMessage>
             </FormControl>
-            <Button isDisabled={formik.isValid === false} type="submit" colorScheme="purple" width="full">
+            <Button isDisabled={formik.isValid === false} type="submit" colorScheme="teal" width="full">
               Submit  {isLoading && <FontAwesomeIcon icon={faSpinner} spinPulse style={ {marginLeft: '5px'}} />}
             </Button>
           </VStack>
