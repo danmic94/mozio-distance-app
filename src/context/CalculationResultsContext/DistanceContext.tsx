2d465ae2ba687075718de46f603757b0b4d34fd8
Dan Nicolae Mic <micdan1994@gmail.com>
2023-06-25 20:46:12 +0300

    [MOZ-1] Added context state variables to create a list of results for in between cities.

--- src/context/CalculationResultsContext/DistanceContext.tsx
+++ src/context/CalculationResultsContext/DistanceContext.tsx
@@ -1,10 +1,17 @@
 import { createContext } from 'react';
 
 interface DistanceCalculationContext {
+    betweenCities: Array<[string, string, number]>;
+    setBetweenCities: Function;
     total: number;
     setTotal: Function;
 }
 
-const CalculationResultsContext = createContext<DistanceCalculationContext>({ total: 0, setTotal: () => {} });
+const CalculationResultsContext = createContext<DistanceCalculationContext>({
+    total: 0,
+    setTotal: () => {},
+    betweenCities: [],
+    setBetweenCities: () => {},
+});
 
 export default CalculationResultsContext;
--- src/context/CalculationResultsContext/DistanceContextProvider.tsx
+++ src/context/CalculationResultsContext/DistanceContextProvider.tsx
@@ -3,12 +3,15 @@ import CalculationResultsContext from './DistanceContext';
 
 const CalculationResultsProvider: React.FC<PropsWithChildren> = props => {
     const [totalDistance, setTotalDistance] = useState(0);
+    const [distanceInBetweenCities, setDistanceInBetweenCities] = useState([]);
 
     return (
         <CalculationResultsContext.Provider
             value={{
                 total: totalDistance,
                 setTotal: setTotalDistance,
+                betweenCities: distanceInBetweenCities,
+                setBetweenCities: setDistanceInBetweenCities,
             }}>
             {props.children}
         </CalculationResultsContext.Provider>
