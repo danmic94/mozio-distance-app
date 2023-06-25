6aa5dc5e89b176c7eae937d101da2eb63fddd670
Dan Nicolae Mic <micdan1994@gmail.com>
2023-06-25 23:25:29 +0300

    [MOZ-1] Changing browser royter to hash router (comply to SPA for githubpages).

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import HomePageComponent from './pages/Home';
 import SearchPageComponent from './pages/Search';
 import CalculationResultsProvider from './context/CalculationResultsContext/DistanceContextProvider';
 import FormContextProvider from './context/FormContext/FormContextProvider';
 // I have to create a form context so data on the form can be rendered on both sides
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/',
         element: <HomePageComponent />,
